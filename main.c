3a0d808cdc69b8904a2bf211fb8cf067f495eab2
Windi159 <107599426+Windi159@users.noreply.github.com>
2023-12-04 14:24:12 +0900

    Update main.c
    im idiot sandwich

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <windows.h>
+
+#define EMPTY '_'
 
 struct canvas
 {
-    char user_input = ' ';
+    char user_input;
     char array[20][71];
 };
 
 void printing(struct canvas *show_setting)
 {
-    for(int col = 0; col <= 20; col++)
+    for(int col = 0; col <= 21; col++)
     {
         if(col <= 1)
             printf("**");
 
         else
-            printf("%2d", col - 1);
+        {
+            if(col <= 10)
+                printf("0");
 
-        for(int low = 0; low <= 70; low++)
+            printf("%d", col - 1);
+        }
+
+
+        for(int low = 0; low < 70; low++)
         {
             if(col == 0)
                 printf("%d", (low + 1) % 10);
@@ -27,7 +36,7 @@ void printing(struct canvas *show_setting)
                 printf("*");
 
             else
-                printf("%c", show_setting -> array[col][low]);
+                printf("%c", show_setting -> array[col - 2][low]);
         }
         printf("\n");
     }
@@ -42,13 +51,13 @@ void initialize(struct canvas *array)
 {
     for(int col = 0; col <= 19; col++)
         for(int low = 0; low <= 70; low++)
-            array -> array[col][low] = '_';
+            array -> array[col][low] = EMPTY;
 }
 
 int main() {
     struct canvas setting;
 
-    char user_input, mode_select;
+    char mode_select;
     int x, y;
 
     initialize(&setting);
@@ -58,36 +67,34 @@ int main() {
 
         while(true)
         {
-            printf("\n사용할 기능을 선택해주세요.\n");
+            printf("\n             사용할 기능을 선택해주세요.\n");
             printf("1.그리기 2.지우기 3.저장하기 4.불러오기 5.새 화면 6.종료\n");
 
-            scanf("%c", &mode_select);
+            scanf(" %c", &mode_select);
 
             if (mode_select == '1')
             {
                 printf("어떤 문자를 표시하시겠습니까?\n");
-                scanf("%c", &user_input);
+                scanf(" %s", &setting.user_input);
 
-                printf("메뉴를 골라주세요.\n");
-                printf("1.위치 지정 2.선 3.도형\n");
+                printf("어디에 문자를 표시하시겠습니까?\n입력 예시 : 가로 세로 ");
+                scanf(" %d %d", &x, &y);
 
-                scanf("%d", &drow_mode);
-
-                printf("어디에 문자를 표시하시겠습니까?\n 입력 예시 : 가로 세로\n");
-                scanf("%d %d", &x, &y);
-
-                setting.array[x][y] = user_input;
+                setting.array[y - 1][x - 1] = setting.user_input;
 
                 break;
             }
+
+
             else if (mode_select == '2')
             {
-                printf("어디에 문자를 표시하시겠습니까?\n 입력 예시 : 가로 세로\n");
-                scanf("%d %d", &x, &y);
+                printf("어디에 문자를 표시하시겠습니까?\n입력 예시 : 가로 세로 ");
+                scanf(" %d %d", &x, &y);
 
-                setting.array[x][y] = ' ';
+                setting.array[y - 1][x - 1] = EMPTY;
                 break;
             }
+
 
             else if (mode_select == '3')
             {
@@ -116,6 +123,6 @@ int main() {
         if(mode_select == '6')
             break;
 
-
+        system("cls");
     }
 }
