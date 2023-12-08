88b9035b3606b119a26b864b6fd220d3742a44c0
Windi159 <107599426+Windi159@users.noreply.github.com>
2023-12-08 13:36:54 +0900

    Update main.c
    저능 아에요

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <io.h>
 
 #define EMPTY ' '
 
@@ -41,24 +42,119 @@ void printing(struct canvas *show_setting)
     }
 }
 
-void save_file(struct canvas *save_setting)
+void crete_txt(char *file_name, int *length)
+{
+    if(*length <= 4)
+    {
+        *(file_name + 1 + *length) = '.';
+        *(file_name + 2 + *length) = 't';
+        *(file_name + 3 + *length) = 'x';
+        *(file_name + 4 + *length) = 't';
+
+        *length += 4;
+    }
+
+    else
+    {
+        if(*(file_name - 3 + *length) != '.' && *(file_name - 2 + *length) != 't' && *(file_name - 1 + *length) != 'x' && *(file_name + *length) != 't')
+        {
+            if(*length > 77)
+                realloc(file_name, -(*length - 81) + 81);
+
+            *(file_name + *length - 3) = '.';
+            *(file_name + *length - 2) = 't';
+            *(file_name + *length - 1) = 'x';
+            *(file_name + *length) = 't';
+
+            *length += -(*length - 80) + 80;
+        }
+    }
+}
+
+void save_file(struct canvas *save_setting, char *named_file, int size)
 {
     FILE* user_text_file;
-    char user_named_file[] = {};
+    char input[1420] = {};
+    char name[size];
 
-    printf("파일 이름을 정해주세요\n");
-    scanf("%s", user_named_file);
+    for(int i = 0; i <= size; i++)
+        name[i] = *(named_file + i);
 
-    user_text_file = fopen(user_named_file, "w");
+    user_text_file = fopen(named_file, "w");
+
     for(int col = 0; col < 20; col++)
-        fwrite(save_setting -> array[col], 1, sizeof(char), user_text_file);
+        for(int low = 0; low <= 70; low++)
+            input[(71 * col) + low] = save_setting -> array[col][low];
+
+    input[1419] = '\0';
+
+    fputs(input, user_text_file);
+    fclose(user_text_file);
+}
+
+void naming_file(struct canvas *file_setting)
+{
+    char *user_named_file;
+    char override;
+    int len = 0;
+
+    user_named_file = calloc(81 ,  sizeof(char));
+
+    printf("파일 이름을 정해주세요. 이름의 최대 길이는 80입니다.\n");
+    scanf( "%s", user_named_file);
+
+    while(user_named_file[len] != '\0')
+        len++;
+
+    char file_name = crete_txt(user_named_file, &len);
+
+    if(_access(user_named_file, 0) != 0)
+        save_file(file_setting, user_named_file, len);
+
+    else
+    {
+        while(true)
+        {
+            printf("동일한 파일명이 존재합니다. 덮어 쓰시겠습니까?(y, n)\n");
+            scanf(" %c", &override);
+
+            if(override == 'y' || override == 'Y')
+            {
+                save_file(file_setting, user_named_file, len);
+                break;
+            }
+
+            else if(override == 'n' || override == 'N')
+            {
+                printf("다른 이름을 입력해주세요.\n");
+                naming_file(file_setting);
+                break;
+            }
+
+            else
+                continue;
+        }
+    }
+
 }
 
 void initialize(struct canvas *array)
 {
     for(int col = 0; col <= 19; col++)
-        for(int low = 0; low <= 70; low++)
+    {
+        for(int low = 0; low <= 69; low++)
+        {
             array -> array[col][low] = EMPTY;
+        }
+
+        array -> array[col][70] = '\n';
+    }
+}
+
+void load_file()
+{
+    FILE* loading_file;
+    char
 }
 
 int main() {
@@ -105,7 +201,7 @@ int main() {
 
             else if (mode_select == '3')
             {
-                save_file(&setting);
+                naming_file(&setting);
                 break;
             }
 
